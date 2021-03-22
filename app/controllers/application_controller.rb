35a1b5fe2b03d25388e818fa50af26c878990a9b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-22 01:23:21 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     if params[:username] == "" || params[:password] == ""
       redirect '/failure'
     else
-      user = User.new(username: params[:username], password: params[:password])
+      redirect '/login'
     end
   end
 
