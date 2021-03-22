2a077d0b72a09ce74af3364c936ea4f28da05112
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-22 01:30:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,6 +45,7 @@ class ApplicationController < Sinatra::Base
     else
       user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
       else
         redirect '/failure'
       end
