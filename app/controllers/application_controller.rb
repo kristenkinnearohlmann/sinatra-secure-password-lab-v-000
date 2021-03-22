2a516434dd41453c047b87f2402dbc59fc2e56f2
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-22 01:28:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
-    if params[:username] == ""
+    if params[:username] == "" || params[:password] == ""
       redirect '/failure'
     else
     end
